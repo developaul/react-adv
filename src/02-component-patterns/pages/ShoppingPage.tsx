e0827ba1b75160838ba6da3bd101f53152e02a7a
Paul Chávez <paulchavezromero20@gmail.com>
2021-12-09 20:22:32 -0500

    [update] pre-npm ready

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -18,32 +18,17 @@ export const ShoppingPage = () => {
       <hr />
       <ProductCard
         key={product.id}
-        className='bg-dark text-white'
         initialValues={{
-          count: 4,
-          maxCount: 10
+          count: 6
         }}
         product={product}
       >
         {({ reset, count, increaseBy, isMaxCountReached }) => {
           return (
             <>
-              <ProductImage
-                className='custom-image'
-              />
-              <ProductTitle
-                className='text-white'
-              />
-              <ProductButtons
-                className='custom-buttons'
-              />
-
-              <button onClick={reset}>Reset</button>
-              <button onClick={() => increaseBy(-2)}>-2</button>
-              {(!isMaxCountReached) && (
-                <button onClick={() => increaseBy(2)}>+2</button>
-              )}
-              <span>{count}</span>
+              <ProductImage />
+              <ProductTitle />
+              <ProductButtons />
             </>
           )
         }}
